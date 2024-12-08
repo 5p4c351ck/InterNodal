9adf9f52046f46c83bab8a7e1d9b96a7eb763285
5p4c351ck <kntazanis@gmail.com>
2024-12-08 23:10:42 +0200

    primitive controller logic for testing

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"log"
 	"strings"
+
+	"github.com/5p4c351ck/DHT-InterNodal/dht"
 )
 
 const maxInputLength = 80
@@ -12,17 +15,30 @@ type Controller interface {
 }
 
 type controllerImpl struct {
-	data string
+	command string
+	DHT     dht.DHT
 }
 
 func (controller *controllerImpl) Receive(input string) {
 	input = strings.TrimSpace(input)
-	controller.data = input[:maxInputLength]
+	controller.command = input[:maxInputLength]
 }
 
 func (controller *controllerImpl) Handle() {
-	switch controller.data {
+	switch controller.command {
 	case "init":
+		if controller.DHT != nil {
+			log.Println("A DHT is already initialized")
+			return
+		}
+		d, err := dht.NewDHT("0.0.0.0", "8080")
+		if err != nil {
+			return
+		}
+		controller.DHT = d
+		log.Println("DHT initialized")
+	default:
+		log.Printf("%s is not a valid command", controller.command)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/5p4c351ck/InterNodal
 
 go 1.23.3
+
+require github.com/5p4c351ck/DHT-InterNodal v0.0.0-20241208201909-86e649530954
